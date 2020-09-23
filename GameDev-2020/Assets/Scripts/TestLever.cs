2c890f46bf49f95540f9d5c4a7bbbe2f758c2fb3
jsantana29 <santanarijo29@gmail.com>
2020-09-22 21:43:20 -0400

    Test lever

---
+++ GameDev-2020/Assets/Scripts/TestLever.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestLever : MonoBehaviour
+{
+    public bool activate;
+    public bool activated;
+    // Start is called before the first frame update
+    void Start()
+    {
+        activate = false;
+        activated = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // if (Input.GetButtonDown("Laser"))
+        // {
+        //     activate = true;
+        //     Debug.Log("Laser");
+        //     //activate = false;
+        // }
+        // else
+        // {
+        //     activate = false;
+        // }
+    }
+
+    private void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && Input.GetButtonDown("Laser"))
+        {
+            Debug.Log("Success");
+
+
+        }
+
+
+
+    }
+
+
+}
