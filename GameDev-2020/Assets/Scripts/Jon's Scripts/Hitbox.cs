3d3451547a480c8c6652292084e3ed1a517e5e77
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2020-09-22 21:04:17 -0400

    Updates to Hitbox

--- GameDev-2020/Assets/Scripts/Jon's Scripts/Hitbox.cs
+++ GameDev-2020/Assets/Scripts/Jon's Scripts/Hitbox.cs
@@ -11,6 +11,7 @@ public class Hitbox : MonoBehaviour
     [SerializeField] private Color hitBoxClosedColor = Color.red;
     [SerializeField] private Color hitBoxOpenColor = Color.yellow;
     [SerializeField] private Color hitboxCollidingColor = Color.green;
+    private IHitboxResponder responder = null;
     private ColliderState currentState = ColliderState.Closed;
 
     public enum ColliderState 
@@ -20,9 +21,39 @@ public class Hitbox : MonoBehaviour
         Colliding
     }
 
-    void Update()
+    public void HitboxUpdate()
     {
-        
+        if (currentState != ColliderState.Closed)
+        {
+            Collider2D[] colliders;
+
+            if (!useCircle)
+            {
+                colliders = Physics2D.OverlapBoxAll(transform.position, boxSize, transform.eulerAngles.z, hitLayer);
+            }
+            else 
+            {
+                colliders = Physics2D.OverlapCircleAll(transform.position, radius, hitLayer);
+            }
+
+
+            for (int i = 0; i < colliders.Length; i++)
+            {
+                if (responder != null)
+                {
+                    responder.CollideWith(colliders[i]);
+                }
+            }
+
+            currentState = colliders.Length > 0 ? ColliderState.Colliding : ColliderState.Open;
+        }
+
+
+    }
+
+    public void SetResponder(IHitboxResponder newResponder) 
+    {
+        responder = newResponder;
     }
 
     public void StartCheckingCollisions()
@@ -62,6 +93,13 @@ public class Hitbox : MonoBehaviour
 
         Gizmos.matrix = Matrix4x4.TRS(transform.position, transform.rotation, transform.localScale);
 
-        Gizmos.DrawCube(Vector3.zero, new Vector2(boxSize.x * halfExtentMultiplier, boxSize.y * halfExtentMultiplier));
+        if (!useCircle)
+        {
+            Gizmos.DrawCube(Vector3.zero, new Vector2(boxSize.x * halfExtentMultiplier, boxSize.y * halfExtentMultiplier));
+        }
+        else 
+        {
+            Gizmos.DrawSphere(Vector3.zero, radius);
+        }
     }
 }
