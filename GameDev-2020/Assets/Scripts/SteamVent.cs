4c29d336b8bd663832e53a259f61d1337f51cac4
SUNTheBaer <padron1999@yahoo.com>
2020-09-24 20:57:07 -0400

    Hit detection working

--- GameDev-2020/Assets/Scripts/SteamVent.cs
+++ GameDev-2020/Assets/Scripts/SteamVent.cs
@@ -2,13 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SteamVent : MonoBehaviour, IHitable
+public class SteamVent : MonoBehaviour
 {
     private bool beginVenting = true;
     [SerializeField] GameObject ventHitbox = null;
     [SerializeField] float timeToStopVenting = 0;
     [SerializeField] float timeToStartVenting = 0;
-    [SerializeField] int damageDealt = 0;
     private void Update()
     {
         if (beginVenting)
@@ -23,14 +22,5 @@ public class SteamVent : MonoBehaviour, IHitable
         ventHitbox.SetActive(false);
         yield return new WaitForSeconds(timeToStartVenting);
         beginVenting = true;
-    }
-
-    private void OnTriggerEnter2D()
-    {
-        Hit(damageDealt);
-    }
-    public void Hit(int damage)
-    {
-        
     }
 }
