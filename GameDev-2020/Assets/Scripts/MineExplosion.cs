18fb408eefa3929ba67fc5528904bd5c7f8bba70
Charles King <charles.king.1@hotmail.com>
2020-10-08 20:13:06 -0400

    Created a timed landmine that explodes with a small delay upon the character colliding with the landmine object
    Still missing the landmine sprite and animation which I am working on, and knockback script still has to be added as well as adding the health manager script

---
+++ GameDev-2020/Assets/Scripts/MineExplosion.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MineExplosion : MonoBehaviour
+{
+    public GameObject explosion;
+    public bool timerStarted = false;
+    private float time = 0f;
+    [SerializeField] float timeAmount = 2;
+
+
+    // Start is called before the first frame upd
+
+     private void OnTriggerEnter2D(Collider2D other){
+         if(other.tag == "Player"){
+
+         
+                if(!timerStarted){
+                    timerStarted = true;
+                }
+         }
+
+        // if(other.tag == "Player"){ //noramlly tags for players is Player
+        //     GameObject e =  Instantiate(explosion) as GameObject;
+        //     e.transform.position = transform.position;
+        //     Destroy(explosion.gameObject);
+        //     this.gameObject.SetActive(false);
+        //     //   other.GetComponent<Rigidbody2D>().AddForce(0.0f, -2.0f);
+        // }
+    }
+
+        // private void explosionTrig(Collider2D other){
+        //      if(other.tag == "Player"){ //noramlly tags for players is Player
+        //     GameObject e =  Instantiate(explosion) as GameObject;
+        //     e.transform.position = transform.position;
+        //     Destroy(explosion.gameObject);
+        //     this.gameObject.SetActive(false);
+        //      }
+        // }
+
+        void Update(){
+            if (timerStarted)
+            {
+            time += Time.deltaTime;
+            
+                if (time >= timeAmount)
+                {
+                GameObject e =  Instantiate(explosion) as GameObject;
+                e.transform.position = transform.position;
+                Destroy(explosion.gameObject);
+                this.gameObject.SetActive(false);
+                }
+            }
+        }
+
+}
