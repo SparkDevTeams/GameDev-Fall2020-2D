b60e399cc20f44575e3cd8a8721ec6868c4a7e3c
Jonathan Muniz <33111715+Jo-Mu@users.noreply.github.com>
2020-10-13 21:07:15 -0400

    Added dimension shift dash

--- GameDev-2020/Assets/Scripts/Jon's Scripts/PlayerMovement.cs
+++ GameDev-2020/Assets/Scripts/Jon's Scripts/PlayerMovement.cs
@@ -14,6 +14,7 @@ public class PlayerMovement : MonoBehaviour
     private Rigidbody2D rb;
     private PlayerJump jump;
     private PlayerState playerState;
+    private DimensionManager dimension;
     private DashDirection direction = DashDirection.None;
     private float dashTimer = 0.0f;
     private float dashBuffer = 0.0f;
@@ -40,6 +41,7 @@ public class PlayerMovement : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();
         jump = GetComponent<PlayerJump>();
         playerState = GetComponent<PlayerState>();
+        dimension = GetComponent<DimensionManager>();
         normalGravity = rb.gravityScale;
     }
 
@@ -93,6 +95,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             dashTimer = dashTime;
+            dimension.executeShift(dashTime);
             stockedDashes--;
             playerState.SetDashing(true);
 
