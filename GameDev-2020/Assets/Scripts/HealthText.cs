3408b52d7cc94c4bf3ac3c22bcbe0dd3bcbdf2bc
jsantana29 <santanarijo29@gmail.com>
2020-10-17 11:02:34 -0400

    Added test UI and made a demo

---
+++ GameDev-2020/Assets/Scripts/HealthText.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthText : MonoBehaviour
+{
+    private HealthManager health;
+    private Text text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        health = FindObjectOfType<HealthManager>();
+        text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        text.text = health.getShieldHealth().ToString();
+    }
+}
