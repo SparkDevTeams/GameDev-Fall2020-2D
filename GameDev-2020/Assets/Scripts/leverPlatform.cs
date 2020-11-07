3a8da3e6564635d8b11866d841fc07edefb22880
Edward <brogaming1012@gmail.com>
2020-11-07 14:05:20 -0500

    importing lever

---
+++ GameDev-2020/Assets/Scripts/leverPlatform.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class leverPlatform : MonoBehaviour
+{
+    public Transform pos1;
+    public Transform pos2;
+    public bool nodePos;
+    public float speed;
+
+    Vector3 nextPos;
+
+    public leverBehavior currentLever;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //currentLever=FindObjectOfType<leverBehavior>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void FixedUpdate(){
+        if(currentLever.getNodeLever()==true){
+            nextPos=pos2.position;
+        }
+        if(currentLever.getNodeLever()==false){
+            nextPos=pos1.position;
+        }
+
+        transform.position=Vector3.MoveTowards(transform.position, nextPos, speed*Time.deltaTime);
+    }
+}
